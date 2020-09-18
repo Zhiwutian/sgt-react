7706ef6862d79f48862abc489f9c4c59165a0cb6
Brett Albright <balbright@learningfuze.com>
2020-09-18 22:53:42 +0000

    completed view average grade

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
-export default () => {
+export default function Header(props) {
   return (
-    <header className="text-center">
-      <h1>Student Grade Table</h1>
+    <header className="d-flex text-center justify-content-around">
+      <h1 className="col-6">Student Grade Table</h1>
+      <h3 className="col-6"> Average:<span className="badge badge-secondary">{props.averageGrade}</span></h3>
     </header>
   );
-};
+}
